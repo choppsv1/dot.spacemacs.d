1fbe2278cb71c17688a64e04da3ecd17fefc2494
Christian E. Hopps <chopps@gmail.com>
2016-01-14 04:28:39 -0500

    new font

--- init.el
+++ init.el
@@ -139,8 +139,8 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
    ;; size to make separators look not too crappy.
-   dotspacemacs-default-font '("Menlo"
-                               :size 15
+   dotspacemacs-default-font '("Deja Vu Sans Mono"
+                               :size 16
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
