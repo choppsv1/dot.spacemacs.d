2a7437c36a468358abd75d1eb52e969be407dcdf
Christian E. Hopps <chopps@gmail.com>
2018-02-01 07:56:01 -0500

    latest

--- local-lisp/generic-lisp.el
+++ local-lisp/generic-lisp.el
@@ -60,6 +60,16 @@
                       (set-window-configuration wnd))))
       (error "no more than 2 files should be marked"))))
 
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+
 ;;-------------------------------
 ;; Disabled commands (not many)
 ;;-------------------------------
