b38364f3ed9de015248409f3dfc3a2f6343889e6
Christian Hopps <chopps@gmail.com>
2020-12-21 04:56:34 +0000

    add rlk

--- init.el
+++ init.el
@@ -181,7 +181,7 @@ This function should only modify configuration layer settings."
            yang-pyang-extra-args "--max-line-length=79")
      )
    ;; These systems get full development packages -- the slowest load
-   chopps-dev-systems '("cmf-xe-1" "tops" "hp13" "labnh" "slk" "dak"))
+   chopps-dev-systems '("cmf-xe-1" "tops" "hp13" "labnh" "rlk" "slk" "dak"))
 
   (cond ((eq system-type 'darwin)
          (setq chopps-layers (append chopps-layers osx-layers)))
@@ -1132,6 +1132,7 @@ layers configuration. You are free to put any user code."
    ((or (string-prefix-p "cmf-" (system-name))
         (string-prefix-p "labnh" (system-name))
         (string-prefix-p "slk" (system-name))
+        (string-prefix-p "rlk" (system-name))
         (string-prefix-p "builder" (system-name))
         (string-prefix-p "hp13" (system-name))
         )
